84b067162bbf0421a41023b75e9ba90ec4390b82
Stuwert <stuart@urback.net>
2015-11-08 15:45:56 -0700

    Added second variation for payment not received. introduced dates.

--- application.js
+++ application.js
@@ -1,5 +1,23 @@
 $(document).ready(function(){
 
+
+	$('.total_amount').on('click', function (){
+
+		if($(this).find('.notification').text() == "Payment Received!"){
+
+			$(this).css('background-color', '#ED474A');
+			$(this).find('.notification').text('Payment Remaining: $60');
+			$('.total p').css('color', "white");
+
+		}else{
+			$(this).css('background-color', '#4C9F70');
+			$(this).find('.notification').text('Payment Received!');
+			$('.total p').css('color', "#363635");
+		}
+
+	});
+
+
 	var details = $(".hidden").detach();
 
 	$(".zoom").on('click', function(){
